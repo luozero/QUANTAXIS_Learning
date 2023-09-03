ad16dc31b02e10297237d78405bf2fd176843598
luozero@126.com <luozero@126.com>
2023-09-03 13:22:20 +0000

    update market value

--- QUANTAXIS/QAData/data_marketvalue.py
+++ QUANTAXIS/QAData/data_marketvalue.py
@@ -35,13 +35,22 @@ def QA_data_calc_marketvalue(data, xdxr):
                                        ['shares_after',
                                         'liquidity_after']].dropna()
     res = pd.concat([data, mv], axis=1)
+
+    res = res.sort_index(level = 'date')
+
     res = res.assign(
         shares=res.shares_after.groupby(level=1).fillna(method='ffill'),
         lshares=res.liquidity_after.groupby(level=1).fillna(method='ffill')
     ).sort_index()
-    return res.assign(mv=res.close*res.shares*10000, liquidity_mv=res.close*res.lshares*10000)\
+
+    res = res.assign(mv=res.close*res.shares*10000, liquidity_mv=res.close*res.lshares*10000)\
               .drop(['shares_after', 'liquidity_after'], axis=1)\
               .loc[(slice(data.index.remove_unused_levels().levels[0][0],data.index.remove_unused_levels().levels[0][-1]),slice(None)),:]
+    
+    res = res.dropna()
+    res = res.drop_duplicates()
+    
+    return res
 
 def QA_data_marketvalue(data):
 
